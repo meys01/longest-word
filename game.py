1f94cd85ab510420b194533507c5e278e285c256
meys01 <37961705+meys01@users.noreply.github.com>
2020-01-21 11:46:14 +0100

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
